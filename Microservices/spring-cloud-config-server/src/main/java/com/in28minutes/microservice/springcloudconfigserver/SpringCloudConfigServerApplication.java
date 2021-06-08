87e3670ec186b3cfe0d0dc7e3a7f4bac0e196461
Lakshmi-Priya-M <lakshmi.moh1297@gmail.com>
2021-06-08 21:41:07 +0530

    Currency-Exchange-Service - Initial Commit
    Till Setting up dynamic port in the response.

--- Microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ Microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.in28minutes.microservice.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // Whenever we create a springcloudconfig, this annotation must be used
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
